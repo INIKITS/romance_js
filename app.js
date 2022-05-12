9ec3cbf799e8ee51d09357262e5493fed7e71c07
Dakota Solis <dakota.h.solis@gmail.com>
2022-05-11 22:14:12 -0500

    Added generateWordPairs funtion. Only returns one word currently, need to figure out how to add more than one value to the same key using a loop.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let text = "Ever since I left the city, you, you, you You and me we just don't g
 function parseText(textString){
     let textArray = [];
     let punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-    let whiteSpace = /\s/g;
+    // let whiteSpace = /\s/g;
     let noPunc = text.replace(punctuation, '').toLowerCase();
     console.log(noPunc);
     
@@ -13,4 +13,24 @@ function parseText(textString){
     return textArray;
 }
 
-parseText(text);
+
+function generateWordPairs(text){
+    let markovChain = {};
+    let tempText = "";
+    let textArray = parseText(text);
+ 
+    for (let i = 0; i < textArray.length; i++){
+        if (textArray[i+1] === undefined){
+            return markovChain;
+        }
+        
+        markovChain[textArray[i]] = textArray[i+1];
+
+
+    }
+    console.log(markovChain);
+    
+}
+
+let wordPairs = generateWordPairs(text);
+console.log(wordPairs);
