ea1e8516ac506d5b686d6cc92178f05d63ef2958
Dakota Solis <dakota.h.solis@gmail.com>
2022-05-12 23:04:18 -0500

    Added statements to check if undefined and if there is a duplicate key in object. Have not figured out how to push duplicates into array just yet.

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 let text = "Ever since I left the city, you, you, you You and me we just don't get along";
 
-function parseText(textString){
-    let textArray = [];
-    let punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-    // let whiteSpace = /\s/g;
-    let noPunc = text.replace(punctuation, '').toLowerCase();
-    console.log(noPunc);
-    
-    textArray = noPunc.split(" ")
-    console.log(textArray)
+function parseText(textString) {
+  let textArray = [];
+  let punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+//   let whiteSpace = /\s/g;
+  let noPunc = text.replace(punctuation, "").toLowerCase();
+//   console.log(noPunc);
 
-    return textArray;
+  textArray = noPunc.split(" ");
+//   console.log(textArray);
+
+  return textArray;
 }
 
+function generateWordPairs(text) {
+  let markovChain = {};
+  let tempText = "";
+  let textArray = parseText(text);
 
-function generateWordPairs(text){
-    let markovChain = {};
-    let tempText = "";
-    let textArray = parseText(text);
- 
-    for (let i = 0; i < textArray.length; i++){
-        if (textArray[i+1] === undefined){
-            return markovChain;
+  for (let i = 0; i < textArray.length; i++) {
+      if (textArray[i + 1] === undefined) {
+          return markovChain;
         }
-        
-        markovChain[textArray[i]] = textArray[i+1];
+        if (textArray[i] in markovChain){
+            let duplicate = textArray[i];
+            console.log(markovChain[duplicate])
+            console.log(textArray[i])
+            console.log(duplicate)
+            // duplicate.push(markovChain);
 
+            markovChain[textArray[i]].push(duplicate);
+        }
+    markovChain[textArray[i]] = [textArray[i + 1]];
 
-    }
-    console.log(markovChain);
-    
+  }
+
 }
 
 let wordPairs = generateWordPairs(text);
