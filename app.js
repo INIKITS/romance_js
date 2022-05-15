339abcfc9f9b8468210d9664feb9d53629881220
Dakota Solis <dakota.h.solis@gmail.com>
2022-05-14 21:46:08 -0500

    Very close to finishing. added generatePoem and most of writeLine functions. Still need to figure out how to stop at word count and add a check if next word is undefined

--- app.js
+++ app.js
@@ -4,19 +4,14 @@ let text =
 function parseText(textString) {
   let textArray = [];
   let punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-  //   let whiteSpace = /\s/g;
   let noPunc = text.replace(punctuation, "").toLowerCase();
-  //   console.log(noPunc);
-
   textArray = noPunc.split(" ");
-  //   console.log(textArray);
 
   return textArray;
 }
 
 function generateWordPairs(text) {
   let markovChain = {};
-  let tempText = "";
   let textArray = parseText(text);
 
   for (let i = 0; i < textArray.length; i++) {
@@ -24,29 +19,86 @@ function generateWordPairs(text) {
       return markovChain;
     }
     if (textArray[i] in markovChain) {
-      let duplicate = textArray[i+1];
-      // console.log(markovChain[duplicate]);
-      // console.log(textArray[i]);
-      // console.log(duplicate);
-      // duplicate.push(markovChain);
+      let duplicate = textArray[i + 1];
       let index = markovChain[textArray[i]];
       index.push(duplicate);
-     
-    }else {
-    markovChain[textArray[i]] = [textArray[i + 1]];
+    } else {
+      markovChain[textArray[i]] = [textArray[i + 1]];
     }
   }
 }
 
-function writeLine(markovChain, wordCount){
-  for (let i = 0; i < wordCount; i++){
+function writeLine(markovChain, wordCount) {
+  // console.log(markovChain);
+  let keys = Object.keys(markovChain);
+  console.log(keys); 
+  let startingWord = randomWord(keys);
+  console.log(startingWord)
+  let line = [];
+
+  for(let i = 1; i < wordCount; i++){
+    let nextPos = markovChain[startingWord];
+    console.log(nextPos);
+    let newWord = randomWord(keys);
+    console.log(newWord)
+    startingWord = markovChain[nextPos];
+    line.push(nextPos);
+    if (line.length > wordCount){
+      continue;
+    }
+    line.push( randomWord(startingWord));
+    console.log(randomWord(startingWord))
+
+
     
   }
+  // console.log(line);
+  return line.join(" ");
 }
 
-function randomWord(markovChain, word){
-
+function randomWord(word) {
+  let max = Math.floor(word.length);
+  let randomNum = Math.floor(Math.random() * max);
+  return word[randomNum];
 }
 
-let wordPairs = generateWordPairs(text);
-console.log(wordPairs);
+function generatePoem(corpus, numLines) {
+  let poem = "";
+  corpus = parseText(corpus);
+  let wordPairs = generateWordPairs(corpus);
+  for (let i = 0; i < numLines; i++) {
+    poem += writeLine(wordPairs, 3) + "\n";
+  }
+  return poem;
+}
+
+console.log(generatePoem(text, 1));
+
+
+
+
+// let newLine = "";
+// let tempLine = ""
+// // startingWord = tempLine;
+// let keys = Object.keys(markovChain);
+// let startingWord = markovChain[keys[(keys.length * Math.random()) << 0]];
+// // console.log(startingWord)
+// newLine += startingWord[0];
+// let nextWord = randomWord(startingWord);
+// for (let i = 0; i < wordCount; i++) {
+//   // console.log(nextWord)
+//   if (!nextWord){
+//     nextWord = randomWord(startingWord);
+//   }else {
+  
+//   newLine += nextWord + " ";
+//   }
+//   // tempLine = startingWord;
+//   // newLine += randomWord(tempLine)
+//   // startingWord = tempLine;
+
+//   // if (newLine === undefined) {
+//   //   newLine += randomWord(startingWord) + " ";
+//   // }
+// }
+// return newLine;
